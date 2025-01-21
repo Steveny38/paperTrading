22f6f82727ed3aec09b08858ce28be9c248fbe21
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2025-01-20 22:16:13 -0500

    eslint route.ts

--- papertrade/app/api/stock/[symbol]/route.ts
+++ papertrade/app/api/stock/[symbol]/route.ts
@@ -100,7 +100,7 @@ async function getStockHistory(symbol: string, timeframe: string){
 
 
 export async function POST(request: Request, { params }: { params: { symbol: string } }){
-    const {symbol} = params
+    const {symbol} = await params
 
     const timeframe = await request.json()
 
